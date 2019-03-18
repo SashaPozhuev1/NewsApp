8370e826a92176905b5aabdef2565858852d3e3c
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2019-03-19 00:21:24 +0200

    Create SourceDTO.java

---
+++ data/SourceDTO.java
@@ -0,0 +1,11 @@
+package data;
+
+public class SourceDTO {
+    String id;
+    String name;
+    String description;
+    String url;
+    String category;
+    String language;
+    String country;
+}
